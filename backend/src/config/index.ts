b1520036d850b706b8f642e4963a4e2998a602d1
C-gyeltshen <02230279.cst@rub.edu.bt>
2025-06-14 18:26:37 +0600

    initial commit

---
+++ backend/src/config/index.ts
@@ -0,0 +1,12 @@
+
+export const databaseConfig = {
+  client: 'pg',
+  version: '12',
+  connection: {
+    host: process.env.DATABASE_HOST,
+    port: Number(process.env.DATABASE_PORT),
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+  }
+}
