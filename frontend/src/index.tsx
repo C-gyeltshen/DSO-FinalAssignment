d6feb16006d18a6d3f49928d06c39d49bcfd57f7
C-gyeltshen <02230279.cst@rub.edu.bt>
2025-06-18 01:48:27 +0600

    wip

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './i18n'
 
 import App from './App'
-import ErrorBoundary from 'components/ErrorBoundary'
+import ErrorBoundary from './components/ErrorBoundary';
 
 
 ReactDOM.render(
